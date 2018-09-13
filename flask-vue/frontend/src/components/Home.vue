9da6b260a4e776c849a1a20e4c3b4a5c2f6fb81b
rio <redrose1994@163.com>
2018-09-13 09:16:53 +0800

    v1.0

---
+++ flask-vue/frontend/src/components/Home.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div>
+    <p>Home page</p>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <button @click="getRandom">New random number</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      // this.randomNumber = this.getRandomInt(1, 100)
+      this.randomNumber = this.getRandomFromBackend()
+    },
+    getRandomFromBackend () {
+      axios.get('http://127.0.0.1:5000/api/random')
+        .then(response => {
+          this.randomNumber = response.data.randomNumber
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
---
+++ flask-vue/frontend/src/router/index.js
@@ -0,0 +1,44 @@
+/*
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
+*/
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const routerOptions = [
+  {
+    path: '/',
+    component: 'Home'
+  },
+  { path: '/about',
+    component: 'About'
+  },
+  { path: '*', component: 'NotFound' }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
+
+Vue.use(Router)
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
